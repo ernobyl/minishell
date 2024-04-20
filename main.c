52b04e956ddb50e560e679e0b3ec779a2c7e8fd9
root <root@DESKTOP-E8CT236>
2024-04-20 11:50:30 +0300

    testing readline and signal handling

--- main.c
+++ main.c
@@ -3,21 +3,35 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h> // (remember to compile with -lreadline)
+#include <signal.h>
+
+volatile sig_atomic_t	exit_flag = 0;
+
+void	handle_signal(int sig)
+{
+	if (sig == SIGQUIT || sig == SIGHUP || sig == SIGTERM)
+		exit_flag = 1;
+	else if (sig == SIGINT)
+		write(1, "\nhandle signal interrupt\n", 25);
+}
 
 int main(void)
 {
 	char	*input;
 
-	while (1)
+	signal(SIGQUIT, handle_signal);
+	signal(SIGINT, handle_signal);
+	signal(SIGHUP, handle_signal);
+	signal(SIGTERM, handle_signal);
+	while (!exit_flag)
 	{
 		input = readline("Input something to echo (or type 'exit' to quit): ");
-		if (strcmp(input, "exit") == 0)
-			break;
+		if (input == NULL || strcmp(input, "exit") == 0)
+			break ;
 		add_history(input);
-		write(STDOUT_FILENO, input, strlen(input));
-		write(STDOUT_FILENO, "\n", 1);
+		write(1, input, strlen(input));
+		write(1, "\n", 1);
 		free(input);
 	}
-
-	return 0;
+	return (0);
 }
