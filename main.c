8fc743c6315b412dd5cf6e989c867ed97d233b5b
Erno Michelsson <emichels@c2r3p10.hive.fi>
2024-04-22 11:30:39 +0300

    added pwd

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: emichels <emichels@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 10:18:45 by emichels          #+#    #+#             */
-/*   Updated: 2024/04/22 10:28:11 by emichels         ###   ########.fr       */
+/*   Updated: 2024/04/22 11:20:23 by emichels         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <readline/readline.h>
-#include <readline/history.h> // (remember to compile with -lreadline)
+#include <readline/history.h>
 #include <signal.h>
+#include <errno.h>
 
 volatile sig_atomic_t	exit_flag = 0;
 
@@ -26,6 +27,47 @@ void	handle_signal(int sig)
 		exit_flag = 1;
 	else if (sig == SIGINT)
 		write(1, "\nhandle signal interrupt\n", 25);
+}
+
+void	double_buf_size(char *str, size_t buf_size)
+{
+	if (errno == ERANGE)
+	{
+		buf_size *= 2;
+		free(str);
+	}
+	else
+	{
+		perror("pwd: getcwd failed");
+		free(str);
+		return ;
+	}
+}
+
+void	pwd_builtin(void)
+{
+	size_t	buf_size;
+	char	*cwd;
+
+	buf_size = 256;
+	cwd = NULL;
+	cwd = malloc(buf_size);
+	if (cwd == NULL)
+	{
+		perror("malloc failed");
+		return ;
+	}
+	if (getcwd(cwd, buf_size) != NULL)
+	{
+		printf("%s\n", cwd);
+		free(cwd);
+		return ;
+	}
+	else
+	{
+		double_buf_size(cwd, buf_size);
+		return ;
+	}
 }
 
 int main(void)
@@ -41,6 +83,8 @@ int main(void)
 		input = readline("Input something to echo (or type 'exit' to quit): ");
 		if (input == NULL || ft_strcmp(input, "exit") == 0)
 			break ;
+		if (ft_strcmp(input, "pwd") == 0)
+			pwd_builtin();
 		add_history(input);
 		printf("%s\n", input);
 		free(input);
