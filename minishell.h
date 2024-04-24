2d93fb1d6629f009eacb0ef3d7a814cbf5d37b85
k2matu <kstt90@gmail.com>
2024-04-24 08:48:46 +0300

    working on parsing

--- minishell.h
+++ minishell.h
@@ -6,20 +6,29 @@
 /*   By: kmatjuhi <kmatjuhi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 11:16:17 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/04/22 11:20:48 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/04/23 13:05:58 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-# include <unistd.h>
-# include "libft/libft.h"
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <readline/history.h>
-# include <signal.h>
+#include <unistd.h>
+#include "libft/libft.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+#include <errno.h>
+
+typedef struct s_struct
+{
+	char *type;
+	char *value;
+	struct s_struct *prev;
+	struct s_struct *next;
+} t_struct;
 
 int	parsing(char *input);
 
--- parsing.c
+++ parsing.c
@@ -6,11 +6,28 @@
 /*   By: kmatjuhi <kmatjuhi@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 11:15:37 by kmatjuhi          #+#    #+#             */
-/*   Updated: 2024/04/22 11:20:22 by kmatjuhi         ###   ########.fr       */
+/*   Updated: 2024/04/24 08:48:27 by kmatjuhi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
 int	parsing(char *input)
 {
+	char	**arr;
+	int		i;
+	t_struct *cmd;
+	t_struct *file;
 
+	i = 0;
+	arr = ft_split(input, ' ');
+	if (!arr)
+		return (0);
+	while (arr[i])
+	{
+		// if find < or > or << or >>
+		printf("%s\n", arr[i]);
+		i++;
+	}
+	return (1);
 }
